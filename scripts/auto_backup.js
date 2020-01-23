61381bb3d9f0dd81709783fa4a86145ac5f5bb50
xiaoxiaoxiaoxiaolin <419685858@qq.com>
2020-01-23 15:19:14 +0800

    blog auto backup script's commit

--- scripts/auto_backup.js
+++ scripts/auto_backup.js
@@ -22,7 +22,7 @@ function run() {
             echo('Error: Git commit failed');
             exit(1);
         }
-        if (exec('git push origin master').code !== 0) {
+        if (exec('git push origin master -f').code !== 0) {
             echo('Error: Git push failed');
             exit(1);
         }
